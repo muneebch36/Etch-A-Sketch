11cfee26be67d8e4cc64fb230b671de44d3bdf1b
yomunnib <muneeb.cheema36@gmail.com>
2022-10-24 19:55:21 -0400

    Add etch function that takes user input.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const grid = document.querySelector(".sketch");
-const input = document.querySelector(".etch");
+const etchButton = document.querySelector(".etch");
 const resetButton = document.querySelector(".reset");
 
-createSketch = () => {
+etchSketch = () => {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
@@ -15,11 +15,17 @@ createSketch = () => {
 
     resetButton.addEventListener("click", function() {
         grid.innerHTML = "";
-        // userInput.value = "";
+        userInput.value = "";
         grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
         grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
-        createSketch();
+        etchSketch();
       });
-      
 
-    createSketch();
+      etchButton.addEventListener("click", function() {
+        const userInput = prompt("Etch x Sketch?","16");
+        grid.innerHTML = "";
+        grid.style.setProperty("grid-template-columns", `repeat(${userInput.value}, 2fr)`);
+        grid.style.setProperty("grid-template-rows",`repeat(${userInput.value}, 2fr)`);
+        for (let i = 0; i < 256; i++) {
+      
+etchSketch();
