580c86b1da5e048322e410e05d29307ce81afd6e
yomunnib <muneeb.cheema36@gmail.com>
2022-10-26 23:22:05 -0400

    Adjust HTML class names and finish base js functions.

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 const grid = document.querySelector(".sketch");
 const etchButton = document.querySelector(".etch");
 const resetButton = document.querySelector(".reset");
+let userInput;
+
+function create() {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div); 
+  }
 
 etchSketch = () => {
     for (let i = 0; i < 256; i++) {
-        const div = document.createElement("div");
-        div.classList.add("square");
-        div.addEventListener('mouseover', function(event){
-            event.target.classList.replace("square", "color");
-        })
-        grid.appendChild(div); 
+        create();
     }
     };
 
-    resetButton.addEventListener("click", function() {
-        grid.innerHTML = "";
-        userInput.value = "";
-        grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
-        grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
-        etchSketch();
-      });
-
       etchButton.addEventListener("click", function() {
-        const userInput = prompt("Etch x Sketch?","16");
+        userInput = +prompt("Squares per side?","16");
         grid.innerHTML = "";
-        grid.style.setProperty("grid-template-columns", `repeat(${userInput.value}, 2fr)`);
-        grid.style.setProperty("grid-template-rows",`repeat(${userInput.value}, 2fr)`);
-        for (let i = 0; i < 256; i++) {
-      
+        grid.style.setProperty("grid-template-columns", `repeat(${+userInput}, 2fr)`);
+        grid.style.setProperty("grid-template-rows",`repeat(${+userInput}, 2fr)`);
+        for (let i = 0; i < (+userInput * +userInput); i++) {
+            create();
+        }
+    });
+
+    const square = document.querySelector("div");
+            square.addEventListener("mouseover", function(event) {
+            event.target.classList.replace("square", "color");
+            });
+
+    resetButton.addEventListener("click", function() {
+            grid.innerHTML = "";
+            userInput = "";
+            grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+            grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+            etchSketch();
+            });
+
 etchSketch();
