6e2287c4dd8a973c527fed92c2496f2afac0bef6
yomunnib <muneeb.cheema36@gmail.com>
2022-10-27 14:17:47 -0400

    Add create function and adjust css .sketch values.

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
 const grid = document.querySelector(".sketch");
 const etchButton = document.querySelector(".etch");
 const resetButton = document.querySelector(".reset");
-let userInput;
+const square = document.querySelector("div");
 
-function create() {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    grid.appendChild(div); 
-  }
+function create(rows, cols) {
+  grid.style.setProperty('--grid-rows', rows);
+  grid.style.setProperty('--grid-cols', cols);
+  for (let i = 0; i < (rows * cols); i++) {
+    let div = document.createElement("div");
+    grid.appendChild(div).className = "square";
+  };
+};
 
-etchSketch = () => {
+etchButton.addEventListener("click", function() {
+    userInput = prompt("Squares per side?","16");
+      if (!+userInput) {;
+          return;
+      } else if (+userInput < 100) {
+          grid.innerHTML = "";
+          create(+userInput, +userInput);          
+      } else {
+          alert("please enter a value below 100");
+      }
+});
+
+square.addEventListener("mouseover", function(event) {
+  event.target.classList.replace("square", "color");
+});
+
+resetButton.addEventListener("click", function() {
+    userInput = "";
+    grid.innerHTML = "";
+    create(16,16);
+});
+
+create(16,16);
+
+/* etchSketch = () => {
     for (let i = 0; i < 256; i++) {
         create();
     }
-    };
+    }; */
 
-      etchButton.addEventListener("click", function() {
-        userInput = +prompt("Squares per side?","16");
-        grid.innerHTML = "";
-        grid.style.setProperty("grid-template-columns", `repeat(${+userInput}, 2fr)`);
-        grid.style.setProperty("grid-template-rows",`repeat(${+userInput}, 2fr)`);
-        for (let i = 0; i < (+userInput * +userInput); i++) {
-            create();
-        }
-    });
-
-    const square = document.querySelector("div");
-            square.addEventListener("mouseover", function(event) {
-            event.target.classList.replace("square", "color");
-            });
-
-    resetButton.addEventListener("click", function() {
-            grid.innerHTML = "";
-            userInput = "";
-            grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
-            grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
-            etchSketch();
-            });
-
-etchSketch();
